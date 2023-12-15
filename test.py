e8b191b05cafde93d8a8ac10921b928629124fd6
jan <janbigerl@outlook.de>
2023-12-15 15:48:36 +0100

    updated requirements.txt

--- test.py
+++ test.py
@@ -2,6 +2,8 @@ import tomli
 import tomli_w
 from pprint import pprint
 import debug
+import json
+import datetime
 
 
 def load_write_config(data: dict | None = None, path: str | None = None) -> dict | None:
@@ -15,13 +17,11 @@ def load_write_config(data: dict | None = None, path: str | None = None) -> dict
 
 
 if __name__ == '__main__':
+    # 'https://re.jrc.ec.europa.eu/pvg_tools/en/#api_5.2'
+    path = r"/Users/jan/Downloads/Timeseries_49.520_11.295_SA2_0deg_0deg_2005_2020.json"
+    data = json.load(open(path, "rb+"))
+    print(data["inputs"].keys())
 
-    config = load_write_config()
-    pprint(config)
-
-    config["trv"].update({"ip": "192.168.178.xxx"})
-    config["trv"].update({"test": "zzz"})
-
-    pprint(config)
-    load_write_config(config)
-    # tomli_w.dump(config, open(path, "wb"))
+    for element in (data["outputs"]["hourly"]):
+        print(datetime.datetime.strptime(element["time"], "%Y%m%d:%H%M").strftime("%H:%M - %d.%m.%Y"))
+        print("radiation: ", element["Gb(i)"])
